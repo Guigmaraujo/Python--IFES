5d1d051ef3b6d400c96c3b40189d8a0fa72e1435
Guigmaraujo <131063148+Guigmaraujo@users.noreply.github.com>
2023-09-13 19:58:02 -0300

    Listas

---
+++ listadelista/ativl2.py
@@ -0,0 +1,6 @@
+p = ['guilherme','pedro','renan','josé','tobias','enzo','sergio','ana','joão','rafael','dayane','eduarda']
+v = []
+for i in p:
+    if len(i)>5:
+        v.append(i)
+print(v)
---
+++ listadelista/ativl5.py
@@ -0,0 +1,6 @@
+n = [1,2,3,4,5,6,7,8,9,10]
+soma = 0
+for i in n:
+    soma = soma + i
+
+print(f'A soma de todos os Números do vetor é: {soma}')
